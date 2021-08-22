2a4c3bf970c50015c5c56ba8e7130ed753f4b145
redmine <estou.eu.te.amo@gmail.com>
2021-08-22 19:55:05 +0900

    flowcontrol 9

--- main.go
+++ main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-func Sqrt(x float64) float64 {
-	z := x / 2
-	count := 0
-	for math.Abs(math.Pow(z, 2)-x) > 0.0000000000001 {
-		count++
-		z -= calcurateNumberForAjustZ(x, z)
-	}
-	fmt.Println(count)
-	return z
-}
-func calcurateNumberForAjustZ(x float64, z float64) float64 {
-	return (math.Pow(z, 2) - x) / (z * 2)
-}
-
 func main() {
-	for target := 1.0; target < 100; target++ {
-		fmt.Println(Sqrt(target))
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s.\n", os)
 	}
 }
