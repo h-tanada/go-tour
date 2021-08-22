858888b0c2e37480b0d8f2432f3a73ea0a27dc0e
redmine <estou.eu.te.amo@gmail.com>
2021-08-22 23:57:49 +0900

    moretypes 26

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	pos, neg := adder(), adder()
-	for i := 0; i < 10; i++ {
-		fmt.Println(pos(i), neg(-2*i))
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	morePrevious := -1
+	previous := 1
+	return func() int {
+		result := morePrevious + previous
+		morePrevious = previous
+		previous = result
+		return result
 	}
 }
 
-func adder() func(int) int {
-	sum := 0
-	return func(x int) int {
-		sum += x
-		return sum
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
 	}
 }
