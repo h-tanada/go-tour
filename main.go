3e4de6fdbb2b0beb13535eb8af820be9af11e96b
redmine <estou.eu.te.amo@gmail.com>
2021-08-23 08:53:57 +0900

    methods 1

--- main.go
+++ main.go
@@ -1,23 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-	morePrevious := -1
-	previous := 1
-	return func() int {
-		result := morePrevious + previous
-		morePrevious = previous
-		previous = result
-		return result
-	}
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
 }
