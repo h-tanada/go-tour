e5b6de5d13588567283fd72707da3a4a7a3ea696
redmine <estou.eu.te.amo@gmail.com>
2021-08-22 20:20:51 +0900

    moretypes 5

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ type Vertex struct {
 	Y int
 }
 
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+)
+
 func main() {
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
-	fmt.Println(p)
+	fmt.Println(v1, v2, v3, p)
 }
