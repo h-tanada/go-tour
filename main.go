6e34b5f939a8e05b775d856d46823658d99586eb
redmine <estou.eu.te.amo@gmail.com>
2021-08-22 23:42:46 +0900

    moretypes 23

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	splitS := strings.Split(s, " ")
+	result := make(map[string]int)
+	for _, value := range splitS {
+		result[value]++
+	}
+	return result
+}
 
 func main() {
-	m := make(map[string]int)
-
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+	wc.Test(WordCount)
 }
