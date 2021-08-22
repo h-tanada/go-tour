f973553489f99fb23f094f92ecaa499f8eb798cc
redmine <estou.eu.te.amo@gmail.com>
2021-08-22 23:07:33 +0900

    moretypes 18

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+	for y, _ := range result {
+		result[y] = make([]uint8, dx)
+		for x, _ := range result[y] {
+			result[y][x] = calc(x, y)
+		}
+	}
+	return result
+}
+
+func calc(x, y int) uint8 {
+	return uint8(y ^ x)
+}
 
 func main() {
-	pow := make([]int, 10)
-	for i := range pow {
-		pow[i] = 1 << uint(i)
-	}
-	for _, value := range pow {
-		fmt.Printf("%d\n", value)
-	}
+	pic.Show(Pic)
 }
